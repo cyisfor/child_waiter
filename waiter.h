e73273830cd7c8cab9a11ccc8bb3e4cccd515f4e
autocommit <autocommit>
2017-08-27 20:20:12 +0000

    auto 0 38 384

--- waiter.h
+++ waiter.h
@@ -7,6 +7,7 @@ bool waiter_wait(int signalfd, time_t sec);
 void waiter_drain(int signalfd);
 int waiter_next(int* status);
 int waiter_fork(void);
-int waiter_waitfor(int signalfd, time_t sec, int expected, bool check_status);
+void waiter_check(int status, bool timeout);
+bool waiter_waitfor(int signalfd, time_t sec, int expected, int* status);
 // # child processes still queued
 int waiter_processes(void);
