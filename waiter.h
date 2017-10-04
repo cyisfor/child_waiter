9e83a2d52130fc46f5040a3cc523e0ce0af19396
autocommit <autocommit>
2017-10-04 17:40:15 +0000

    auto 0 121 1054

--- waiter.h
+++ waiter.h
@@ -3,15 +3,18 @@
 #include <signal.h>
 #include <poll.h>
 
-extern sigset_t waiter_sigmask;
-
-int waiter_setup(void);
+const sigset_t* waiter_setup(void);
 void waiter_unblock(void);
-bool waiter_wait(struct pollfd* poll, int npoll, time_t sec);
-void waiter_drain(int signalfd);
+bool waiter_wait(const sigset_t* sigmask, struct pollfd* poll, int npoll, time_t sec);
+// call this to drain a signalfd, and then waiter_next until it returns 0
+void waiter_drain(const sigset_t* sigmask);
 int waiter_next(int* status);
 int waiter_fork(void);
 void waiter_check(int status, bool timeout, int expected);
-bool waiter_waitfor(int signalfd, time_t sec, int expected, int* status);
+// wait for JUST ONE process
+// no other processes may exit during this time.
+// returns true if timeout
+bool waiter_waitfor(sigset_t* sigmask, time_t timeout, int expected, int *status);
+
 // # child processes still queued
 int waiter_processes(void);
